8b008655312809076707ac92212cacb414dbc133
Kill00 <kill00@p.teamvery.dev>
2021-10-27 09:59:21 +0900

    notice system hotfix

--- bot/handler/Slash/공지사항.js
+++ bot/handler/Slash/공지사항.js
@@ -21,19 +21,28 @@ module.exports = {
         const noticeData = JSON.parse(fs.readFileSync(`${process.cwd()}/data/noticeData.json`, 'utf-8'))
         const keys = Object.keys(noticeData)
         const Format = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', fractionalSecondDigits: 2, timeZone: 'asia/seoul', timeZoneName: 'short'}
-        for (let i = 0; i < notice_count; i++) {
+        for (let i = 0; i < keys.length; i++) {
             if (noticeData[keys[i]] !== undefined) {
                 const dateFormat = Intl.DateTimeFormat('ko-KR', Format).format(new Date(Number(keys[i])))
                 noticeList.push(`+ [ ${dateFormat} ]\n${noticeData[keys[i]]}`)
             }
         }
 
+        const reverse = noticeList.reverse();
+
+        let notice_loaded_count
+        if (noticeList.length < notice_count) {
+            notice_loaded_count = noticeList.length
+        } else {
+            notice_loaded_count = notice_count
+        }
+
         const notice = new Discord.MessageEmbed()
         notice.setColor("GOLD")
             .setAuthor("공지사항")
-            .addField(`* 최근 ${noticeList.length}개의 공지사항을 가져옵니다.`, `\`\`\`diff\n${noticeList.join('\n\n')}\`\`\``, false)
+            .addField(`* 최근 ${notice_loaded_count}개의 공지사항을 가져옵니다.`, `\`\`\`diff\n${reverse.slice(0, notice_count).join('\n\n')}\`\`\``, false)
             .setTimestamp()
             .setFooter(`개발자 : ${interaction.client.users.cache.find(user => user.id === config.owner).tag}`)
         await interaction.reply({embeds: [notice], ephemeral: true})
-    },
+    }
 }
